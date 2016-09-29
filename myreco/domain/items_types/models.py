8571c53ee1a51b253ca945c8d368778903f2e45d
Diogo Dutra <dutradda@gmail.com>
2016-09-29 19:52:20 -0300

    created the tests for items types model

--- myreco/domain/items_types/models.py
+++ myreco/domain/items_types/models.py
@@ -22,6 +22,10 @@
 
 
 from myreco.base.models.sqlalchemy_redis import SQLAlchemyRedisModelBase
+from myreco.base.models.base import get_model_schema
+from myreco.base.hooks import before_operation, AuthorizationHook
+from myreco.domain.users.models import UsersModel
+from myreco.domain.constants import AUTH_REALM
 from myreco.exceptions import ModelBaseError
 from jsonschema import Draft4Validator
 from collections import defaultdict
@@ -29,47 +33,54 @@ import sqlalchemy as sa
 import json
 
 
+@before_operation(AuthorizationHook(UsersModel.authorize, AUTH_REALM))
 class ItemsTypesModel(SQLAlchemyRedisModelBase):
     __tablename__ = 'items_types'
     __table_args__ = {'mysql_engine': 'innodb'}
+    __schema__ = get_model_schema(__file__)
 
     id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.Integer, unique=True, nullable=False)
+    name = sa.Column(sa.String(255), unique=True, nullable=False)
     id_names_json = sa.Column(sa.String(255), nullable=False)
     schema_json = sa.Column(sa.Text, nullable=False)
-    engine_id = sa.Column(sa.ForeignKey('engines.id'), nullable=False)
 
     @classmethod
-    def insert(cls, session, objs, commit=True, todict=True):
+    def insert(cls, session, objs, commit=True, todict=True, **kwargs):
         cls._format_input_json(objs)
-        return type(cls).insert(cls, session, objs, commit, todict)
+        objs = type(cls).insert(cls, session, objs, commit, todict, **kwargs)
+        if todict:
+            cls._format_output_json(objs)
+        return objs
 
     @classmethod
     def _format_input_json(cls, objs):
         objs = cls._to_list(objs)
-        for obj in ojbs:
+        for obj in objs:
             id_names = obj.pop('id_names', None)
             if id_names:
                 obj['id_names_json'] = json.dumps(id_names)
 
-            schema = obj.pop('schema', None)
-            if schema:
-                obj['schema_json'] = json.dumps(schema)
+            if 'schema' in obj:
+                obj['schema_json'] = json.dumps(obj.pop('schema'))
 
     @classmethod
-    def update(cls, session, objs, commit=True, todict=True):
+    def update(cls, session, objs, commit=True, todict=True, ids=None, **kwargs):
         cls._format_input_json(objs)
-        return type(cls).update(cls, session, objs, commit, todict)
+        objs = type(cls).update(cls, session, objs, commit, todict, ids, **kwargs)
+        if todict:
+            cls._format_output_json(objs)
+        return objs
 
     @classmethod
-    def get(cls, session, ids=None, limit=None, offset=None, **kwargs):
-        objs = type(cls).get(cls, session, ids, limit, offset, **kwargs)
-        cls._format_output_json(objs)
+    def get(cls, session, ids=None, limit=None, offset=None, todict=True, **kwargs):
+        objs = type(cls).get(cls, session, ids, limit, offset, todict, **kwargs)
+        if todict:
+            cls._format_output_json(objs)
         return objs
 
     @classmethod
     def _format_output_json(cls, objs):
         objs = cls._to_list(objs)
-        for obj in ojbs:
+        for obj in objs:
             obj['id_names'] = json.loads(obj.pop('id_names_json'))
             obj['schema'] = json.loads(obj.pop('schema_json'))
