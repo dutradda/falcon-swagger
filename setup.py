c76847e272b7d607c23756d8a74a5f486edcc8a1
Diogo Dutra <dutradda@gmail.com>
2016-10-09 18:30:44 -0300

    falcon-swagger officially launched

--- setup.py
+++ setup.py
@@ -1,22 +1,63 @@
-# MIT License
+# Copyright 2016 Diogo Dutra
 
-# Copyright (c) 2016 Diogo Dutra
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+#     http://www.apache.org/licenses/LICENSE-2.0
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
+from falconswagger.version import VERSION
+from setuptools import setup
+
+
+long_description = ''
+with open('README.md') as readme:
+    long_description = readme.read()
+
+
+install_requires = []
+with open('requirements.txt') as requirements:
+    install_requires = requirements.readlines()
+
+
+tests_require = []
+with open('requirements-dev.txt') as requirements_dev:
+    tests_require = requirements_dev.readlines()
+
+
+setup(
+    name='falcon-swagger',
+    packages=['falconswagger'],
+    version=VERSION,
+    description='A Falcon Framework extension featuring Swagger, SQLAlchemy and Redis',
+    long_description=long_description,
+    author='Diogo Dutra',
+    author_email='dutradda@gmail.com',
+    url='https://github.com/dutradda/falcon-swagger',
+    download_url='http://github.com/dutradda/falcon-swagger/archive/master.zip',
+    license='MIT',
+    keywords='falcon framework swagger openapi sqlalchemy redis crud',
+    setup_requires=[
+        'pytest-runner==2.9',
+        'setuptools==28.3.0'
+    ],
+    tests_require=tests_require,
+    install_requires=install_requires,
+    classifiers=[
+    	'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
