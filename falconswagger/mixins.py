d04043cfe2bcf94b60f4038ea17fe7d58c480536
Diogo Dutra <dutradda@gmail.com>
2016-12-08 20:38:22 -0200

    added a function on logger mixin to build the name

--- falconswagger/mixins.py
+++ falconswagger/mixins.py
@@ -4,4 +4,7 @@ import logging
 class LoggerMixin(object):
 
     def _build_logger(self):
-        self._logger = logging.getLogger('{}.{}'.format(type(self).__module__, type(self).__name__))
+        self._logger = logging.getLogger(self._build_logger_name())
+
+    def _build_logger_name(self):
+        return '{}.{}'.format(type(self).__module__, type(self).__name__)
--- falconswagger/version.py
+++ falconswagger/version.py
@@ -21,4 +21,4 @@
 # SOFTWARE.
 
 
-VERSION = '0.9.3'
+VERSION = '0.9.4'
